eba65e8548f170ba7b5d0c470680a452460af7d7
Vinod Chandra <vchandra@macbook.local>
2020-03-01 19:29:29 -0500

    added component for json input

--- visualizer/src/App.js
+++ visualizer/src/App.js
@@ -1,7 +1,29 @@
 
 import React from 'react'
+import JSONInput from 'react-json-editor-ajrm';
+import locale from 'react-json-editor-ajrm/locale/en';
 import Visualizer from './components/Visualizer'
+import pkg from '../package.json'
 
-const App = (input) => (<Visualizer input={input}></Visualizer>)
+const App = () => {
+    return (
+        <div>
+            <div>
+                <JSONInput
+                    id='a_unique_id'
+                    placeholder={pkg}
+                    colors={{
+                        string: "#DAA520" // overrides theme colors with whatever color value you want
+                    }}
+                    locale={locale}
+                    height='550px'
+                />
+            </div>
+            <div>
+                <Visualizer src={pkg}></Visualizer>
+            </div>
+        </div>
+    )
+}
 
 export default App;
