f277b494ca0beddcd0f6d6edac88452cdee87276
Vinod Chandra <vchandra@macbook.local>
2020-03-07 14:59:16 -0500

    removed visualizer project

--- visualizer/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Visualizer from './components/Visualizer'
-
-pm.getData((error, response) => {
-    ReactDOM.render(<Visualizer src={response} />, document.getElementById("root"))
-})
